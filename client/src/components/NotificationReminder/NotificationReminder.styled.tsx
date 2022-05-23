93f7ca1d2470a93887efa7c01ab0b7b43811cb9f
Mateusz Kłusek <mateuszklusek@outlook.com>
2022-05-23 17:30:08 +0200

    changed icon for notification reminder

--- client/src/components/NotificationReminder/NotificationReminder.styled.tsx
+++ client/src/components/NotificationReminder/NotificationReminder.styled.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components"
 
 
-export const NotificationReminderContainer = styled.svg`
-   width: 40px;
-   height: 40px;
+export const NotificationReminderContainer = styled.div`
+   width: 45px;
+   height: 45px;
    position: absolute; 
    top: 40px;
    right: 40px;
     
+`
+
+export const Icon = styled.img`
+   width: 45px;
+   height: 45px;
 `
